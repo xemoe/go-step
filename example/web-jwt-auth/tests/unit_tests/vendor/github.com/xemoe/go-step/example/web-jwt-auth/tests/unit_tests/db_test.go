91bcef704c1a3de55e2273165af621694a6758b8
Teerapong ladlee <blckpearl.sheeper@gmail.com>
2017-12-14 17:00:58 +0700

    Add dependencies vendor files

---
+++ example/web-jwt-auth/tests/unit_tests/vendor/github.com/xemoe/go-step/example/web-jwt-auth/tests/unit_tests/db_test.go
@@ -0,0 +1,35 @@
+//
+// db_test.go
+//
+
+package unit_test
+
+import (
+	"github.com/xemoe/go-step/example/web-jwt-auth/models"
+	"testing"
+)
+
+func TestNewSqliteDB_ShouldReturnExpectedValue(t *testing.T) {
+	DB = models.NewSqliteDB(dbname)
+	if DB == nil {
+		t.Error("Failed to new sqlite3 db")
+	}
+}
+
+//
+// Copy from https://github.com/jinzhu/gorm/blob/master/main_test.go
+//
+func TestSetAndGet(t *testing.T) {
+	DB = models.NewSqliteDB(dbname)
+	if value, ok := DB.Set("hello", "world").Get("hello"); !ok {
+		t.Errorf("Should be able to get setting after set")
+	} else {
+		if value.(string) != "world" {
+			t.Errorf("Setted value should not be changed")
+		}
+	}
+
+	if _, ok := DB.Get("non_existing"); ok {
+		t.Errorf("Get non existing key should return error")
+	}
+}
